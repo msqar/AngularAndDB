5adf46602d6f7f0dfb79bdf5c5acfcf048a0b973
Mariano Marzullo <mariano.marzullo@gmail.com>
2013-12-05 15:30:29 -0300

    adding status to error

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -10,14 +10,14 @@ productApp.config(function($routeProvider) {
 });
 
 productApp.controller('productController', function($scope, $http) {
-	$http.get('php/connect.php').
+	$http.get('php/connect2.php').
 	success(function(data, status) {
 		$scope.products = data;
 		console.log($scope.products + ' with status ' + status);
 	}).
 	error(function(data, status, headers, config) {		
 		$scope.error = true;
-		$scope.msg = 'Failed to retrieve data from the database';
+		$scope.msg = 'Failed to retrieve data from the database. Reason: ' + status;
 	});
 })
 
